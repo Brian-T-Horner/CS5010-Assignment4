e46e4513dfa74f5dc68c026300c815ebf4f699f3
Ted Banken <tedbanken@gmail.com>
2023-03-09 16:21:47 -0500

    refactoring and created model for PPMImage

--- src/Model.java
+++ src/Model.java
@@ -1,26 +1,26 @@
 
 public interface Model {
 
-    int loadPPMImage(String imagePath, String newImageName);
+  int loadPPMImage(String imagePath, String newImageName);
 
-    int savePPMImage(String imagePath, String imageName);
+  int savePPMImage(String imagePath, String imageName);
 
-     int getRedscaleImage(String currentImageName, String newImageName);
+  int getRedComponent(String currentImageName, String newImageName);
 
-    int getGreenscaleImage(String currentImageName, String newImageName);
+  int getGreenComponent(String currentImageName, String newImageName);
 
-    int getBluescaleImage(String currentImageName, String newImageName);
+  int getBlueComponent(String currentImageName, String newImageName);
 
-    int flipHorizontal(String currentImageName, String newImageName);
+  int flipHorizontal(String currentImageName, String newImageName);
 
-    int flipVertical(String currentImageName, String newImageName);
+  int flipVertical(String currentImageName, String newImageName);
 
-    int getValueImage(String currentImageName, String newImageName);
+  int getValueImage(String currentImageName, String newImageName);
 
-    int getIntensityImage(String currentImageName, String newImageName);
+  int getIntensityImage(String currentImageName, String newImageName);
 
-    int getLumaImage(String currentImageName, String newImageName);
+  int getLumaImage(String currentImageName, String newImageName);
 
-    int brighten(String currentImageName, String newImageName, int scale);
+  int brighten(String currentImageName, String newImageName, int scale);
 
 }
--- src/PPMImage.java
+++ src/PPMImage.java
@@ -1,33 +1,206 @@
-import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
-/**
- * Interface for manipulating a .ppm image file.
- */
-public interface PPMImage {
+public class PPMImage implements Image {
 
-  boolean isSingleChannel();
+  private final int height;
+  private final int width;
 
-  int [][] getRedComponent();
+  private final int[][] redComponent;
 
-  int [][] getGreenComponent();
+  private final int[][] greenComponent;
 
-  int [][] getBlueComponent();
+  private final int[][] blueComponent;
 
 
-  int getHeight();
+  public PPMImage(int width, int height, int[][] red,
+                  int[][] blue, int[][] green) {
+    this.height = height;
+    this.width = width;
+    this.redComponent = red;
+    this.greenComponent = green;
+    this.blueComponent = blue;
+  }
 
-  int getWidth();
 
-  PPMImage flipHorizontal(String name);
+  @Override
+  public boolean isSingleChannel() {
+    if (Arrays.equals(redComponent, greenComponent) && Arrays.equals(redComponent, blueComponent)) {
+      return true;
+    }
+    return false;
+  }
 
-  PPMImage flipVertical(String name);
+  @Override
+  public int[][] getRedComponent() {
+    return redComponent;
+  }
 
-  PPMImage getValueImage(String name);
+  @Override
+  public int[][] getGreenComponent() {
+    return greenComponent;
+  }
 
-  PPMImage getIntensityImage(String name);
+  @Override
+  public int[][] getBlueComponent() {
+    return blueComponent;
+  }
 
-  PPMImage getLumaImage(String name);
+  @Override
+  public int getHeight() {
+    return height;
+  }
 
-  PPMImage brighten(String name,int scale);
+  @Override
+  public int getWidth() {
+    return width;
+  }
+
+  @Override
+  public Image flipHorizontal() {
+    int[][] red = flipArrayHorizontal(redComponent);
+    int[][] green = flipArrayHorizontal(greenComponent);
+    int[][] blue = flipArrayHorizontal(blueComponent);
+    return new PPMImage(width, height, red, blue, green);
+  }
+
+  private int[][] flipArrayHorizontal(int[][] arr) {
+    int temp;
+    int[][] flippedH = new int[width][height];
+    for (int i = 0; i < getHeight(); i++) {
+      for (int j = 0; j < getWidth() / 2; j++) {
+        temp = arr[getWidth() - j - 1][i];
+        flippedH[getWidth() - j - 1][i] = arr[j][i];
+        flippedH[j][i] = temp;
+      }
+    }
+
+    return flippedH;
+  }
+
+  @Override
+  public Image flipVertical() {
+    int[][] red = flipArrayVertical(redComponent);
+    int[][] green = flipArrayVertical(greenComponent);
+    int[][] blue = flipArrayVertical(blueComponent);
+    return new PPMImage(width, height, red, blue, green);
+  }
+
+  private int[][] flipArrayVertical(int[][] arr) {
+    int temp;
+    int[][] flippedV = new int[width][height];
+    for (int i = 0; i < getWidth(); i++) {
+      for (int j = 0; j < getHeight() / 2; j++) {
+        temp = arr[i][j];
+        flippedV[i][j] = arr[i][getHeight() - 1 - j];
+        flippedV[i][getHeight() - 1 - j] = temp;
+      }
+    }
+    return flippedV;
+  }
+
+
+  @Override
+  public Image getValueImage() {
+    int[][] value = new int[width][height];
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        int redValue = redComponent[i][j];
+        int greenValue = greenComponent[i][j];
+        int blueValue = blueComponent[i][j];
+        int max = Math.max(redValue, greenValue);
+        max = Math.max(max, blueValue);
+        value[i][j] = max;
+      }
+    }
+    return new PPMImage(width, height, value, value, value);
+  }
+
+  @Override
+  public Image getIntensityImage() {
+    int[][] intensity = new int[width][height];
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        int redValue = redComponent[i][j];
+        int greenValue = greenComponent[i][j];
+        int blueValue = blueComponent[i][j];
+        int intensityValue = (int) Math.ceil((double) (redValue + greenValue + blueValue) / 3);
+        if (intensityValue > 255) {
+          intensityValue = 255;
+        } else if (intensityValue < 0) {
+          intensityValue = 0;
+        }
+        intensity[i][j] = intensityValue;
+      }
+    }
+    return new PPMImage(width, height, intensity, intensity, intensity);
+  }
+
+  @Override
+  public Image getLumaImage() {
+    int[][] luma = new int[width][height];
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        int redValue = redComponent[i][j];
+        int greenValue = greenComponent[i][j];
+        int blueValue = blueComponent[i][j];
+        int lumaValue = (int) Math.ceil((double) 0.2126 * redValue + 0.7152 * greenValue + 0.0722 * blueValue);
+        if (lumaValue > 255) {
+          lumaValue = 255;
+        } else if (lumaValue < 0) {
+          lumaValue = 0;
+        }
+        luma[i][j] = lumaValue;
+      }
+    }
+    return new PPMImage(width, height, luma, luma, luma);
+  }
+
+  @Override
+  public Image brighten(int scale) {
+    int[][] red = brightenArray(redComponent, scale);
+    int[][] green = brightenArray(greenComponent, scale);
+    int[][] blue = brightenArray(blueComponent, scale);
+    return new PPMImage(width, height, red, blue, green);
+  }
+
+  private int[][] brightenArray(int[][] arr, int scale) {
+    int[][] brightened = new int[width][height];
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        int val = arr[i][j] + scale;
+        if (val > 255) {
+          val = 255;
+        } else if (val < 0) {
+          val = 0;
+        }
+        brightened[i][j] = val;
+      }
+    }
+    return brightened;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (!(o instanceof PPMImage)) {
+      return false;
+    }
+    PPMImage c = (PPMImage) o;
+
+    return c.getWidth() == this.getWidth() && c.getHeight() == this.getHeight() &&
+            c.blueComponent.equals(blueComponent) && c.greenComponent.equals(greenComponent) &&
+            c.redComponent.equals(redComponent);
+
+  }
+
+  @Override
+  public int hashCode() {
+    return 67499 * this.getWidth() * this.getHeight();
+  }
+
 
 }
+
