578877d11ecf368d09f498b731842fc6256536f5
Brian-T-Horner <horner.br@northeastern.edu>
2023-03-10 11:37:34 -0500

    Added comments for PPMModel

--- src/PPMModel.java
+++ src/PPMModel.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * PPMModel that controls the method of a PPMImage.
+ */
 public class PPMModel implements Model{
 
   Map<String, Image> images = new HashMap<>();
@@ -8,14 +11,27 @@ public class PPMModel implements Model{
   Set<String> commands = new HashSet<>(Arrays.asList("vertical-flip", "horizontal-flip", "greyscale", "brighten",
           "rgb-split", "rgb-combine", "value", "intensity", "luma", "save", "load", "darken"));
 
+  /**
+   * Constructor for PPMModel. No parameters as only one default field.
+   */
   public PPMModel() {
     //only one default field
   }
 
+  /**
+   * Method to return the commands of the PPMModel.
+   * @return A Set of Strings that are the commands of the PPMModel.
+   */
   public Set<String> getCommands() {
     return commands;
   }
 
+  /**
+   * Method to load a PPMImage - calls the PPMImage object method.
+   * @param imagePath The path of the PPMImage to load.
+   * @param newImageName The name to assign to the loaded PPMImage.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int loadImage(String imagePath, String newImageName) {
     Image i = ImageUtil.readIntoPPMImage(imagePath);
@@ -23,6 +39,12 @@ public class PPMModel implements Model{
     return 1;
   }
 
+  /**
+   * Method to save an PPMImage - calls the PPMImage object method.
+   * @param imagePath The path of where to save the PPMImage.
+   * @param imageName The name of the PPMImage to save.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int saveImage(String imagePath, String imageName) {
     Image i = images.get(imageName);
@@ -37,6 +59,12 @@ public class PPMModel implements Model{
     return 1;
   }
 
+  /**
+   * Method to get the red component of an PPMImage - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to get the red component from.
+   * @param newImageName The name to save the resulting PPMImage as.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int getRedComponent(String currentImageName, String newImageName) {
     Image i = images.get(currentImageName);
@@ -48,6 +76,12 @@ public class PPMModel implements Model{
     return 0;
   }
 
+  /**
+   * Method to get the green component of an PPMImage - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to get the green component from.
+   * @param newImageName The name to save the resulting PPMImage as.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int getGreenComponent(String currentImageName, String newImageName) {
     Image i = images.get(currentImageName);
@@ -59,6 +93,12 @@ public class PPMModel implements Model{
     return 0;
   }
 
+  /**
+   * Method to get the blue component of an PPMImage - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to get the blue component from.
+   * @param newImageName The name to save the resulting PPMImage as.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int getBlueComponent(String currentImageName, String newImageName) {
     Image i = images.get(currentImageName);
@@ -70,6 +110,12 @@ public class PPMModel implements Model{
     return 0;
   }
 
+  /**
+   * Method to flip an PPMImage horizontally - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to flip horizontally.
+   * @param newImageName The name to save the resulting PPMImage as.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int flipHorizontal(String currentImageName, String newImageName) {
     Image i = images.get(currentImageName);
@@ -80,6 +126,12 @@ public class PPMModel implements Model{
     return 0;
   }
 
+  /**
+   * Method to flip an PPMImage vertically - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to flip vertically.
+   * @param newImageName The name to save the resulting PPMImage as.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int flipVertical(String currentImageName, String newImageName) {
     Image i = images.get(currentImageName);
@@ -90,6 +142,12 @@ public class PPMModel implements Model{
     return 0;
   }
 
+  /**
+   * Method to get the value image of an PPMImage - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to get the value image from.
+   * @param newImageName The name to save the resulting value image as.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int getValueImage(String currentImageName, String newImageName) {
     Image i = images.get(currentImageName);
@@ -100,6 +158,12 @@ public class PPMModel implements Model{
     return 0;
   }
 
+  /**
+   * Method to get the intensity image of an PPMImage - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to get the intensity image from.
+   * @param newImageName The name to save the resulting intensity image as.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int getIntensityImage(String currentImageName, String newImageName) {
     Image i = images.get(currentImageName);
@@ -110,6 +174,12 @@ public class PPMModel implements Model{
     return 0;
   }
 
+  /**
+   * Method to get the luma image of an PPMImage - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to get the luma image from.
+   * @param newImageName The name to save the resulting luma image as.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int getLumaImage(String currentImageName, String newImageName) {
     Image i = images.get(currentImageName);
@@ -120,6 +190,13 @@ public class PPMModel implements Model{
     return 0;
   }
 
+  /**
+   * Method to brighten or darken an PPMImage - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to brighten or darken.
+   * @param newImageName The name to save the resulting PPMImage as.
+   * @param scale The scale to brighten or darken the PPMImage.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int brighten(String currentImageName, String newImageName, int scale) {
     Image i = images.get(currentImageName);
@@ -134,7 +211,14 @@ public class PPMModel implements Model{
     return 0;
   }
 
-
+  /**
+   * Method to split an PPMImage into its rgb components - calls the PPMImage object method.
+   * @param currentImageName The name of the PPMImage to split into its rgb components.
+   * @param redImageName The name to save the resulting red component PPMImage as.
+   * @param greenImageName The name to save the resulting green component PPMImage as.
+   * @param blueImageName The name to save the resulting blue component PPMImage as.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int rgbSplit(String currentImageName, String redImageName, String greenImageName, String blueImageName) {
     getRedComponent(currentImageName, redImageName);
@@ -143,7 +227,14 @@ public class PPMModel implements Model{
     return 1;
   }
 
-
+  /**
+   * Method to combine rgb components into one PPMImage.
+   * @param newImageName The name to save the resulting PPMImage as.
+   * @param rImageName The name of the red component PPMImage.
+   * @param gImageName The name of the green component PPMImage.
+   * @param bImageName The name of the blue component PPMImage.
+   * @return 0 for failure, other numbers for success.
+   */
   @Override
   public int rgbCombine(String newImageName, String rImageName, String gImageName, String bImageName) {
     Image redImage = images.get(rImageName);
@@ -161,12 +252,25 @@ public class PPMModel implements Model{
       return 0;
   }
 
+  /**
+   * Method to get the greyscale image of an PPMImage - calls the PPMImage object method.
+   * @param greyScaleComponent The method to get the grey scale of the PPMImage.
+   * @param imageName The PPMImage to get the grey scale image from.
+   * @param newImageName The name to save the resulting grey scale image as.
+   * @return 0 for failure, other numbers for success.
+   */
   //TODO: Implement
   @Override
   public int greyscale(String greyScaleComponent, String imageName, String newImageName) {
     return 1;
   }
 
+  /**
+   * Helper method to compare dimensions of two PPMImages and see height and width are equal.
+   * @param firstImage First PPMImage to compare.
+   * @param secondImage Second PPMImage to compare.
+   * @return True if height and widths are equal, false if otherwise.
+   */
   private boolean compareDimensions(Image firstImage, Image secondImage) {
     return (firstImage.getHeight() == secondImage.getHeight()
         && firstImage.getWidth() == secondImage.getWidth());
