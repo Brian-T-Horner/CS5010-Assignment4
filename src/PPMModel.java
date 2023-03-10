0f9ab547978934c6a119a794a4de0eb0d19b4e18
Brian <horner.br@northeastern.edu>
2023-03-10 14:57:15 -0500

    Fixed comments on PPMModel

--- src/PPMModel.java
+++ src/PPMModel.java
@@ -30,7 +30,6 @@ public class PPMModel implements Model{
    * Method to load a PPMImage - calls the PPMImage object method.
    * @param imagePath The path of the PPMImage to load.
    * @param newImageName The name to assign to the loaded PPMImage.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void loadImage(String imagePath, String newImageName) {
@@ -42,7 +41,6 @@ public class PPMModel implements Model{
    * Method to save an PPMImage - calls the PPMImage object method.
    * @param imagePath The path of where to save the PPMImage.
    * @param imageName The name of the PPMImage to save.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void saveImage(String imagePath, String imageName) {
@@ -60,7 +58,6 @@ public class PPMModel implements Model{
    * Method to get the red component of an PPMImage - calls the PPMImage object method.
    * @param currentImageName The name of the PPMImage to get the red component from.
    * @param newImageName The name to save the resulting PPMImage as.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void getRedComponent(String currentImageName, String newImageName) {
@@ -75,7 +72,6 @@ public class PPMModel implements Model{
    * Method to get the green component of an PPMImage - calls the PPMImage object method.
    * @param currentImageName The name of the PPMImage to get the green component from.
    * @param newImageName The name to save the resulting PPMImage as.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void getGreenComponent(String currentImageName, String newImageName) {
@@ -90,7 +86,6 @@ public class PPMModel implements Model{
    * Method to get the blue component of an PPMImage - calls the PPMImage object method.
    * @param currentImageName The name of the PPMImage to get the blue component from.
    * @param newImageName The name to save the resulting PPMImage as.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void getBlueComponent(String currentImageName, String newImageName) {
@@ -105,7 +100,6 @@ public class PPMModel implements Model{
    * Method to flip an PPMImage horizontally - calls the PPMImage object method.
    * @param currentImageName The name of the PPMImage to flip horizontally.
    * @param newImageName The name to save the resulting PPMImage as.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void flipHorizontal(String currentImageName, String newImageName) {
@@ -119,7 +113,6 @@ public class PPMModel implements Model{
    * Method to flip an PPMImage vertically - calls the PPMImage object method.
    * @param currentImageName The name of the PPMImage to flip vertically.
    * @param newImageName The name to save the resulting PPMImage as.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void flipVertical(String currentImageName, String newImageName) {
@@ -133,7 +126,6 @@ public class PPMModel implements Model{
    * Method to get the value image of an PPMImage - calls the PPMImage object method.
    * @param currentImageName The name of the PPMImage to get the value image from.
    * @param newImageName The name to save the resulting value image as.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void getValueImage(String currentImageName, String newImageName) {
@@ -147,7 +139,6 @@ public class PPMModel implements Model{
    * Method to get the intensity image of an PPMImage - calls the PPMImage object method.
    * @param currentImageName The name of the PPMImage to get the intensity image from.
    * @param newImageName The name to save the resulting intensity image as.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void getIntensityImage(String currentImageName, String newImageName) {
@@ -161,7 +152,6 @@ public class PPMModel implements Model{
    * Method to get the luma image of an PPMImage - calls the PPMImage object method.
    * @param currentImageName The name of the PPMImage to get the luma image from.
    * @param newImageName The name to save the resulting luma image as.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void getLumaImage(String currentImageName, String newImageName) {
@@ -176,7 +166,6 @@ public class PPMModel implements Model{
    * @param currentImageName The name of the PPMImage to brighten or darken.
    * @param newImageName The name to save the resulting PPMImage as.
    * @param scale The scale to brighten or darken the PPMImage.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void brighten(String currentImageName, String newImageName, int scale) {
@@ -196,7 +185,6 @@ public class PPMModel implements Model{
    * @param redImageName The name to save the resulting red component PPMImage as.
    * @param greenImageName The name to save the resulting green component PPMImage as.
    * @param blueImageName The name to save the resulting blue component PPMImage as.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void rgbSplit(String currentImageName, String redImageName, String greenImageName, String blueImageName) {
@@ -220,7 +208,6 @@ public class PPMModel implements Model{
    * @param rImageName The name of the red component PPMImage.
    * @param gImageName The name of the green component PPMImage.
    * @param bImageName The name of the blue component PPMImage.
-   * @return 0 for failure, other numbers for success.
    */
   @Override
   public void rgbCombine(String newImageName, String rImageName, String gImageName, String bImageName) {
@@ -242,9 +229,7 @@ public class PPMModel implements Model{
    * @param greyScaleComponent The method to get the grey scale of the PPMImage.
    * @param imageName The PPMImage to get the grey scale image from.
    * @param newImageName The name to save the resulting grey scale image as.
-   * @return 0 for failure, other numbers for success.
    */
-  //TODO: Implement
   @Override
   public void greyscale(String greyScaleComponent, String imageName, String newImageName) {
     switch (greyScaleComponent) {
